cb5e01b83efc6d419e7b5f631fe02eb7711a7859
dlskrud815 <dlskrud815@gmail.com>
2024-04-29 17:20:40 +0900

    Exercise3

---
+++ Exercise3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int row, col, input;
+	
+
+	cout << "** 2차원 배열 복습 - 행렬 합 **" << endl
+		<< "행과 열의 수를 입력하세요: ";
+	cin >> row >> col;
+
+	cout << endl << "행렬 원소를 입력하세요: " << endl;
+
+	//2차원 동적 배열 선언
+	int** arr = new int* [row];
+	for (int i = 0; i < row; i++)
+	{
+		arr[i] = new int[col];
+	}
+
+	//배열 입력
+	for (int i = 0; i < row; i++)
+	{
+		cout << i << "행: ";
+
+		for (int j = 0; j < col; j++)
+		{
+			cin >> input;
+			arr[i][j] = input;
+		}
+	}
+
+	cout << endl << "각 행의 합: " << endl;
+	for (int i = 0; i < col; i++)
+	{
+		int rowSum = 0;
+		cout << "행 " << i << ": ";
+
+		for (int j = 0; j < row; j++)
+		{
+			rowSum += arr[i][j];
+		}
+		cout << rowSum << endl;
+	}
+
+	cout << endl << "각 열의 합: " << endl;
+	for (int i = 0; i < col; i++)
+	{
+		int colSum = 0;
+		cout << "열 " << i << ": ";
+
+		for (int j = 0; j < row; j++)
+		{
+			colSum += arr[j][i];
+		}
+		cout << colSum << endl;
+
+	}
+
+	return 0;
+}
